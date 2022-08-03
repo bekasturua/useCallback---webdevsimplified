4cbdad47e3d5eba605b6e43cbe982ee11929dc28
Beka Sturua <beqasturua3@gmail.com>
2022-08-03 19:19:20 +0400

    finish useCallback

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ function App() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  const getItems = useCallback(() => {
-    return [number, number + 1, number + 2];
-  }, [number]);
+  const getItems = useCallback(
+    (incrementor) => {
+      return [
+        number + incrementor,
+        number + 1 + incrementor,
+        number + 2 + incrementor,
+      ];
+    },
+    [number]
+  );
 
   const theme = {
     backgroundColor: dark ? "#333" : "#FFF",
